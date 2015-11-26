ecc56ed341607edb5d73f5b398609ee44fd8136c
Dragan Vuletic <vuleticd@gmail.com>
2015-11-26 16:12:44 +0100

    Create Order.php

---
+++ Model/Order.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category    Vuleticd
+ * @package     Vuleticd_OrderHistory
+ * @copyright   Copyright (c) 2015 Vuletic Dragan (http://www.vuleticd.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+ 
+namespace Vuleticd\OrderHistory\Model;
+
+use Vuleticd\OrderHistory\Api\Data\OrderInterface;
+use Magento\Framework\DataObject\IdentityInterface;
+
+class Order  extends \Magento\Framework\Model\AbstractModel implements OrderInterface, IdentityInterface
+{
+	const CACHE_TAG = 'sales_history_order';
+
+    /**
+     * @var string
+     */
+    protected $_cacheTag = 'sales_history_order';
+
+    /**
+     * Prefix of model events names
+     *
+     * @var string
+     */
+    protected $_eventPrefix = 'sales_history_order';
+	
+	/**
+     * Initialize resource model
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('Vuleticd\OrderHistory\Model\ResourceModel\Order');
+    }
+	
+	/**
+     * Return unique ID(s) for each object in system
+     *
+     * @return array
+     */
+    public function getIdentities()
+    {
+        return [self::CACHE_TAG . '_' . $this->getOrderId()];
+    }
+
+    /**
+     * Get ORDER_ID
+     *
+     * @return int|null
+     */
+    public function getOrderId()
+    {
+        return $this->getData(self::ORDER_ID);
+    }
+	
+	/**
+     * Set ORDER_ID
+     *
+     * @param int $order_id
+     * @return \Vuleticd\OrderHistory\Api\Data\OrderInterface
+     */
+    public function setOrderId($order_id)
+    {
+        return $this->setData(self::ORDER_ID, $order_id);
+    }
+}
