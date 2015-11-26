af03c31eac3b3857029bafa4bbcc83b686930f9d
Dragan Vuletic <vuleticd@gmail.com>
2015-11-26 16:54:28 +0100

    Create Old.php

---
+++ Controller/Order/Old.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category    Vuleticd
+ * @package     Vuleticd_OrderHistory
+ * @copyright   Copyright (c) 2015 Vuletic Dragan (http://www.vuleticd.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+ 
+namespace Vuleticd\OrderHistory\Controller\Order;
+
+use \Magento\Framework\App\Action\Action;
+
+class Old extends Action
+{
+    /** @var  \Magento\Framework\View\Result\Page */
+    protected $resultPageFactory;
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     */
+    public function __construct(\Magento\Framework\App\Action\Context $context,
+                                \Magento\Framework\View\Result\PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Old order view, shows a list of recent blog posts.
+     *
+     * @return \Magento\Framework\View\Result\PageFactory
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
