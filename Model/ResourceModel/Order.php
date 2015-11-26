8e1b0d3d31aa061fb18ed50eaf5616fe90a901a9
Dragan Vuletic <vuleticd@gmail.com>
2015-11-26 16:18:26 +0100

    Create Order.php

---
+++ Model/ResourceModel/Order.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category    Vuleticd
+ * @package     Vuleticd_OrderHistory
+ * @copyright   Copyright (c) 2015 Vuletic Dragan (http://www.vuleticd.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+ 
+namespace Vuleticd\OrderHistory\Model\ResourceModel;
+
+/**
+ * Order History mysql resource
+ */
+class Order extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
+{
+	/**
+     * Construct
+     *
+     * @param \Magento\Framework\Model\ResourceModel\Db\Context $context
+     * @param string|null $resourcePrefix
+     */
+    public function __construct(
+        \Magento\Framework\Model\ResourceModel\Db\Context $context,
+        $resourcePrefix = null
+    ) {
+        parent::__construct($context, $resourcePrefix);
+    }
+
+    /**
+     * Initialize resource model
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('sales_history_order', 'order_id');
+    }
+}
